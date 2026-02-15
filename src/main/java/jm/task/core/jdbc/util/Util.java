e414073bedbaadcb8776ec32f8eb718cc6c8b9bd
SirWhiteMice <105564413+SirWhiteMice@users.noreply.github.com>
2026-02-15 19:57:28 +0300

    Выполнено. Все тесты проходит.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
-public class Util {
-    // реализуйте настройку соеденения с БД
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class Util {
+
+    private static final String URL_KEY = "db.url";
+    private static final String USER_key = "db.username";
+    private static final String PASSWORD_KEY = "db.password";
+
+    private Util() {
+    }
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            return DriverManager.getConnection(
+                    PropertiesUtil.getProperty(URL_KEY),
+                    PropertiesUtil.getProperty(USER_key),
+                    PropertiesUtil.getProperty(PASSWORD_KEY)
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
