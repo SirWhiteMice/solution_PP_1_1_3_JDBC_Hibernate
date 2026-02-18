a079486198fb1fc8f64924d65e449f1e9fc21da8
SirWhiteMice <il.a.tkachev.personal@gmail.com>
2026-02-18 18:33:38 +0300

    Удалены JDBC сущности. Готово к реализаци Hibernate

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,62 +1,23 @@
 package jm.task.core.jdbc.model;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users", schema = "public")
 public class User {
     @Id
     private Long id;
-
-    @Column
     private String name;
-
-    @Column
     private String lastName;
-
-    @Column
     private Byte age;
-
-    public User() {
-
-    }
-
-    public User(String name, String lastName, Byte age) {
-        this.name = name;
-        this.lastName = lastName;
-        this.age = age;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Byte getAge() {
-        return age;
-    }
-
-    public void setAge(Byte age) {
-        this.age = age;
-    }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,22 +6,6 @@ import java.sql.SQLException;
 
 public final class Util {
 
-    private static final String URL_KEY = "db.url";
-    private static final String USER_key = "db.username";
-    private static final String PASSWORD_KEY = "db.password";
-
     private Util() {
-    }
-
-    public static Connection getConnection() throws SQLException {
-        try {
-            return DriverManager.getConnection(
-                    PropertiesUtil.getProperty(URL_KEY),
-                    PropertiesUtil.getProperty(USER_key),
-                    PropertiesUtil.getProperty(PASSWORD_KEY)
-            );
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
